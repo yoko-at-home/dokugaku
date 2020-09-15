713ab93e551ec276aee0a003d1b68c81e98c8f59
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2020-09-15 20:36:39 +0900

    Lesson4-27 関数のリファクタリングまで済み

--- src/Lesson/book_log.php
+++ src/Lesson/book_log.php
@@ -1,40 +1,44 @@
 <?php
-
 function createReview()
 {
     echo '読書ログを追加してください' . PHP_EOL;
     echo '書籍名：';
-    $title = trim(fgets(STDIN));
+    $review['title'] = trim(fgets(STDIN));
     echo '著者名：';
-    $author = trim(fgets(STDIN));
+    $review['author'] = trim(fgets(STDIN));
     echo '読書状況（未読、読んでる、読了）：';
-    $status = trim(fgets(STDIN));
+    $review['status'] = trim(fgets(STDIN));
     echo '評価（5点満点の整数）：';
-    $rating = trim(fgets(STDIN));
+    $review['rating'] = trim(fgets(STDIN));
     echo '感想：';
-    $comment = trim(fgets(STDIN));
-    echo '登録が完了しました' . PHP_EOL . PHP_EOL;
+    $review['comment'] = trim(fgets(STDIN));
+    echo '登録が完了しました' . PHP_EOL;
+
     return [
-        'title' => $title,
-        'author' => $author,
-        'status' => $status,
-        'rating' => $rating,
-        'comment' => $comment,
+        'title' => $review['title'],
+        'author' => $review['author'],
+        'status' => $review['status'],
+        'rating' => $review['rating'],
+        'comment' => $review['comment']
     ];
 }
 
-function listReview($reviews)
+function listReviews($reviews)
 {
+    echo '--------------------------'  . PHP_EOL;
     echo '読書ログを表示します' . PHP_EOL;
 
     foreach ($reviews as $review) {
         echo '書籍名：' . $review['title'] . PHP_EOL;
         echo '著者名：' . $review['author'] . PHP_EOL;
         echo '読書状況：' . $review['status'] . PHP_EOL;
-        echo '評価：' . $review['rating'] . PHP_EOL;
+        echo '評価（5点満点の整数）：' . $review['rating'] . PHP_EOL;
         echo '感想：' . $review['comment'] . PHP_EOL;
+        echo '--------------------------'  . PHP_EOL;
     }
 }
+
+$reviews = [];
 
 while (true) {
     echo '1. 読書ログを登録' . PHP_EOL;
@@ -44,9 +48,11 @@ while (true) {
     $num = trim(fgets(STDIN));
     if ($num === '1') {
         $reviews[] = createReview();
+        echo '---------------------------' . PHP_EOL;
     } elseif ($num === '2') {
-        listReview($reviews);
+        listReviews($reviews);
     } elseif ($num === '9') {
+        echo '---------------------------' . PHP_EOL;
         break;
     }
 }
