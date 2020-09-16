6b1108bcb012632dd4b40876f1f86a858b9473b4
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2020-09-16 11:21:22 +0900

    mysqlへの接続後取得した結果（連想配列）をvar_exportで確認

--- src/Lesson/mysql.php
+++ src/Lesson/mysql.php
@@ -7,10 +7,22 @@ if (!$link) {
 }
 
 echo 'データベースに接続できました' . PHP_EOL;
-return $link;
+// return $link;
 
 // MySQL文の命令
+$sql = 'SELECT id, book_title, author, status, rating, comment, created_at FROM review';
+$results = mysqli_query($link, $sql); //成功したらmysqli_resultオブジェクトを返す
+while ($review = mysqli_fetch_assoc($results)) //mysqli_queryの結果行を連想配列で取得する
+{
+    var_export($review);
+    // echo '書籍名：' . $review['book_title'] . PHP_EOL;
+    // echo '著者名：' . $review['author'] . PHP_EOL;
+    // echo '読書状況（1＝未読,2＝読んでる,3＝読了）：' . $review['status'] . PHP_EOL;
+    // echo '評価（5点満点の整数）：' . $review['rating'] . PHP_EOL;
+    // echo '感想：' . $review['comment'] . PHP_EOL;
+}
 
+mysqli_free_result($results); //メモリの解放
 // 切断
 mysqli_close($link);
 echo 'データベースとの接続を切断できました' . PHP_EOL;
