0f1a27626c634d88282a6fc6b9dad51d1aa40a6c
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2020-10-21 01:24:06 +0900

    フォームの初期値を設定

--- src/Lesson/new.php
+++ src/Lesson/new.php
@@ -1,4 +1,11 @@
 <?php
+$review = [
+    'title' => '',
+    'author' => '',
+    'status' => '未読',
+    'rating' => '',
+    'comment' => ''
+];
 $errors = [];
 
 include 'views/new.php';
--- src/Lesson/views/new.php
+++ src/Lesson/views/new.php
@@ -21,36 +21,36 @@
         <!-- ここにエラー処理 ends-->
         <div>
             <label for="title">書籍名</label>
-            <input type="text" name="title" id="title">
+            <input type="text" name="title" id="title" value="<?php echo $review['title'] ?>">
         </div>
         <div>
             <label for="author">著者名</label>
-            <input type="text" name="author" id="author">
+            <input type="text" name="author" id="author" value="<?php echo $review['author'] ?>">
         </div>
         <div>
             <label>読書状況</label>
             <div>
                 <div>
-                    <input type="radio" name="status" id="status1" value="未読">
+                    <input type="radio" name="status" id="status1" value="未読" <?php echo ($review['status'] === '未読' ? 'checked' : '') ?>>
                     <label for="status1">未読</label>
                 </div>
                 <div>
-                    <input type="radio" name="status" id="status2" value="読んでる">
+                    <input type="radio" name="status" id="status2" value="読んでる" <?php echo ($review['status'] === '読んでる' ? 'checked' : '') ?>>
                     <label for="status2">読んでる</label>
                 </div>
                 <div>
-                    <input class="form-check-input" type="radio" name="status" id="status3" value="読了">
+                    <input class="form-check-input" type="radio" name="status" id="status3" value="読了" <?php echo ($review['status'] === '読了' ? 'checked' : '') ?>>
                     <label for="status3">読了</label>
                 </div>
             </div>
         </div>
         <div>
             <label for="rating">評価（5点満点の整数）</label>
-            <input type="number" name="rating" id="rating">
+            <input type="number" name="rating" id="rating" value="<?php echo $review['rating'] ?>">
         </div>
         <div>
             <label for="comment">感想</label>
-            <textarea type="text" name="comment" id="comment" rows="10"></textarea>
+            <textarea type="text" name="comment" id="comment" rows="10" value="<?php echo $review['comment'] ?>"></textarea>
         </div>
         <button type="submit">登録する</button>
     </form>
