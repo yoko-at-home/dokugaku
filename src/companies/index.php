a5a139ce673a31345806bf6318a4dd12dbee7a79
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2020-11-01 17:41:44 +0900

    テーブルが空の時のロジックを追加

--- src/companies/index.php
+++ src/companies/index.php
@@ -1,4 +1,23 @@
 <?php
+require_once __DIR__ . '/lib/mysqli.php';
+
+function listCompanies($link)
+{
+    $companies = [];
+    $sql = 'SELECT name, establishment_date, founder FROM companies;
+    ';
+    $results = mysqli_query($link, $sql);
+
+    while ($company = mysqli_fetch_assoc($results)) {
+        $companies[] = $company;
+    }
+    mysqli_free_result($results);
+    return $companies;
+}
+
+$link = dbConnect();
+$companies = listCompanies($link);
+
 
 $title = "会社情報の一覧";
 $content = __DIR__ . '/views/index.php';
--- src/companies/views/index.php
+++ src/companies/views/index.php
@@ -1,18 +1,20 @@
 <h1 class="h2 text-dark mt-4 mb-4"><?php echo $title; ?></h1>
-<a href="new.php">会社情報の登録</a>
+<a href="new.php">会社情報を登録する</a>
 <main>
-    <section>
-        <h2>株式会社メルカリ</h2>
-        <div>創業：2013年 | 代表：山田進太郎</div>
-    </section>
-    <section>
-        <h2>株式会社メルカリ</h2>
-        <div>創業：2013年 | 代表：山田進太郎</div>
-    </section>
-    <section>
-        <h2>株式会社メルカリ</h2>
-        <div>創業：2013年 | 代表：山田進太郎</div>
-    </section>
+    <?php if (count($companies) > 0) : ?>
+        <?php foreach ($companies as $company) : ?>
+            <section>
+                <h2>
+                    <?php echo $company['name']; ?>
+                </h2>
+                <div>創業：<?php echo $company['establishment_date']; ?>年&nbsp;|&nbsp;
+                    代表：<?php echo $company['founder']; ?></div>
+            </section>
+        <?php endforeach; ?>
+    <?php else : ?>
+        <p>会社情報が登録されていません。</p>
+    <?php endif; ?>
+
 </main>
 
 </body>
