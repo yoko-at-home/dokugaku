07f5208d8d23cea57e47a7cbc5f7bf24458732ee
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2020-11-01 21:16:20 +0900

    companiesにxss対策を実装

--- src/companies/index.php
+++ src/companies/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/lib/escape.php';
 require_once __DIR__ . '/lib/mysqli.php';
 
 function listCompanies($link)
--- src/companies/views/index.php
+++ src/companies/views/index.php
@@ -5,10 +5,10 @@
         <?php foreach ($companies as $company) : ?>
             <section>
                 <h2>
-                    <?php echo $company['name']; ?>
+                    <?php echo escape($company['name']); ?>
                 </h2>
-                <div>創業：<?php echo $company['establishment_date']; ?>年&nbsp;|&nbsp;
-                    代表：<?php echo $company['founder']; ?></div>
+                <div>創業：<?php echo escape($company['establishment_date']); ?>年&nbsp;|&nbsp;
+                    代表：<?php echo escape($company['founder']); ?></div>
             </section>
         <?php endforeach; ?>
     <?php else : ?>
